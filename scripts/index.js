45591e0d6dde7b8fcf69d929a35fb694073bc7a5
JuiceboxJones <jderhalli@gmail.com>
2019-01-28 08:37:50 -0800

    initial commit

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,16 @@
-/* global shoppingList, store */
+'use strict';
+
+/* global shoppingList, store, api */
 
 $(document).ready(function() {
   shoppingList.bindEventListeners();
   shoppingList.render();
+
 });
 
 store.items.push(Item.create('apples'));
+
+api.getItems()
+  .then(res => console.log(res));
+
+console.log(api.BASE_URL);
