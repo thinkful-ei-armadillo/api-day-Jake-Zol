7e044bf896e746177185d6262a76e7ee4aae421a
JuiceboxJones <jderhalli@gmail.com>
2019-01-28 09:31:49 -0800

    third step complete

--- scripts/api.js
+++ scripts/api.js
@@ -3,14 +3,31 @@
 const api = (function () {
   const BASE_URL = 'https://thinkful-list-api.herokuapp.com/jake-zol';
   const getItems = function() {
-    // const url = `${BASE_URL}/items`;
-    // return fetch(url)
-    //             .then(response => response);
-    // return Promise.resolve('A successful response!');
+    const url = `${BASE_URL}/items`;
+    return fetch(url)
+      .then(response => response);
+    //return Promise.resolve('A successful response!');
   };
 
+  const createItem = function(name){
+    let newItem = JSON.stringify({
+      name
+    });
+    const url = `${BASE_URL}/items`;
+    return fetch(url,
+      { method: 'POST',
+        headers: { 'Content-Type': 'application/json'},
+        body: newItem
+      })
+      .then(response => response);  
+  };
+
+  
+
   return {
-    getItems
+    getItems,
+    createItem
+
   };
 }());
 
--- scripts/index.js
+++ scripts/index.js
@@ -14,4 +14,16 @@ store.items.push(Item.create('apples'));
 //   .then(response => response.json())
 //   .then(data => console.log(data));
 
+
+// api.createItem('pears')
+//   .then(res => res.json())
+//   .then((newItem) => {
+//     return api.getItems();
+//   })
+//   .then(res => res.json())
+//   .then((items) => {
+//     console.log(items);
+//   });
+
+
 console.log(api.BASE_URL);
